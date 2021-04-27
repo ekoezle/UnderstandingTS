dee27cf35a02c5ad040a95650e94ec6a33cd11f5
Eugen Közle <eugen.koezle@salt-solutions.de>
2021-04-27 08:09:57 +0200

    42

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,9 @@ const userName = 'Ich';
 let testVar: string = 'Test';
 
 
-const button = document.querySelector('#test-button') as HTMLButtonElement;
+const button = document.querySelector('#test-button')! as HTMLButtonElement;
 
 button.addEventListener('click', () => {
     console.log('Button clicked');
 }); 
 
-const map = new Map();
