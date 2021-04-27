b0cf48e897de0c9a51a6a04e31a5790afadce00a
Eugen Közle <eugen.koezle@salt-solutions.de>
2021-04-27 08:12:49 +0200

    43

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+const userName = 'Ich';
+let testVar = 'Test';
+const button = document.querySelector('#test-button');
+button.addEventListener('click', () => {
+    console.log('Button clicked');
+});
+//# sourceMappingURL=app.js.map
